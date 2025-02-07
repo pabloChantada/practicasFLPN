dcb342cf201f097f49815b95324fb38333dc1471
pabloChantada <pablo.chantada@udc.es>
2025-02-07 17:16:44 +0100

    Organizacion

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,26 @@
-python -m venv FLPN
+#!/bin/bash
 
-pip install regex
+# Definir el nombre del entorno virtual correctamente (sin espacios en la asignación)
+ENV_DIR="FLPN"
+
+# Verificar si el entorno virtual ya existe
+if [ -d "$ENV_DIR" ]; then
+  echo "The environment already exists."
+  exit 0
+fi
+
+echo "Creating Python environment: $ENV_DIR"
+python3 -m venv "$ENV_DIR"
+
+# Activar el entorno virtual para instalar los paquetes necesarios
+source "$ENV_DIR/bin/activate"
+
+# Librerías de FLPN
+echo "Installing dependencies for $ENV_DIR"
+pip install regex numpy pandas matplotlib
+
+echo "Environment created successfully"
+
+# Añadir el entorno al .gitignore si no está presente
+grep -qxF "$ENV_DIR/" .gitignore || echo "$ENV_DIR/" >> .gitignore
+
